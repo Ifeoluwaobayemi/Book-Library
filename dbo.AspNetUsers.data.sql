687c35466d8cce280b8acaad0ebea151e85b38b4
Ifeoluwaobayemi <ifeoluwaobayemi00@gmail.com>
2023-08-27 03:41:01 +0100

    book library updated

--- dbo.AspNetUsers.data.sql
+++
@@ -1,4 +0,0 @@
-﻿Update AspNetUsers Set Email = 'ifeoluwaobayemi01@gmail.com' Where Id = 'b5ab8f9d-2ed3-490a-968f-5513daccbcd7'
-Update AspNetUsers Set NormalizedEmail = 'IFEOLUWAOBAYEMI01@GMAIL.COM' Where Id = 'b5ab8f9d-2ed3-490a-968f-5513daccbcd7'
-Update AspNetUsers Set UserName = 'ifeoluwaobayemi01@gmail.com' Where Id = 'b5ab8f9d-2ed3-490a-968f-5513daccbcd7'
-Update AspNetUsers Set NormalizedUserName = 'IFEOLUWAOBAYEMI01@GMAIL.COM' Where Id = 'b5ab8f9d-2ed3-490a-968f-5513daccbcd7'
